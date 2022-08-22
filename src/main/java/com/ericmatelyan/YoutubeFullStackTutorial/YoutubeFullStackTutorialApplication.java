d978c77c78608cd43c0b5bf8f8811740c1af0e22
samurainstein <eric.matelyan@gmail.com>
2022-08-22 16:38:02 -0400

    Initial commit

---
+++ src/main/java/com/ericmatelyan/YoutubeFullStackTutorial/YoutubeFullStackTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.ericmatelyan.YoutubeFullStackTutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubeFullStackTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoutubeFullStackTutorialApplication.class, args);
+	}
+
+}
