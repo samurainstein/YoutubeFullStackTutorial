f5fdc58fe72c678c055fae67aadf4bb29014adb2
samurainstein <eric.matelyan@gmail.com>
2022-08-25 09:59:56 -0400

    Created HeaderComponent and FooterComponent and added to App.js file. Added footer class css to App.css file. React application displays info successfully

--- frontend/react-frontend/src/App.js
+++ frontend/react-frontend/src/App.js
@@ -1,11 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
 import ListEmployeeComponent from "./Components/ListEmployeeComponent";
+import HeaderComponent from "./Components/HeaderComponent";
+import FooterComponent from "./Components/FooterComponent";
 
 function App() {
   return (
-    <div className="container">
-      <ListEmployeeComponent />
+    <div>
+      <HeaderComponent />
+      <div className="container">
+        <ListEmployeeComponent />
+      </div>
+      <FooterComponent />
     </div>
   );
 }
